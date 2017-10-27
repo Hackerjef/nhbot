8d9959525cec6b3bc78bf9b55cdc5198fe2491ca
hackerjef <auguststenz@aol.com>
2017-10-27 12:49:32 -0400

    scraped the checkconfig idea, need to figure new idea, (still not tested) and needs to figure out how to disable and reinable stuff without using alot of if statements

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-// if configs are good, get data.
-// check.run(typeofencoding, name, file direcory, typeofconfig);
-let check = require("./nodeEvents/check.js");
+// if configs are good, get data);
 //auth
-check.auth("utf8", "Authjson", "./config/auth-config.json", "auth");
+const Authjson = require("./config/auth-config.json", "utf8");
 //config
-check.config("utf8", "configjson", "./config/config.json", "main");
+const configjson = require("./config/auth-config.json", "utf8");
 //perm
-check.perms("utf8", "permsjson", "./config/perms.json", "perm");
+const permsjson = require("./config/auth-config.json", "utf8");
 
 //debug
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
-//client.on("debug", (e) => console.info(e));
+client.on("debug", (e) => console.info(e));
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
@@ -43,20 +41,22 @@ client.on("message", (message) => {
   let userrole = "norole";
 
   // if disabled set usr role as disabled and stops permmision system from continuing.
-  if (permsjson.perms == "disabled") userrole = "disabled" break nopermgroups;
-
+  if (permsjson.perms == "disabled") userrole = "disabled";
+  if (permsjson.perms == "disabled") break nopermgroups;
   // get id of admin role (if set up), if not disable admin group and skip to config modrole.
   const adminRole = message.guild.roles.find("name", permsjson.Admingroup);
-  if (adminRole.id == "null") console.error("admin group not found, admin commands disabled") break noadmingroup;
+  if (adminRole.id == "null") console.error("admin group not found, admin commands disabled");
+  if (adminRole.id == "null") break noadmingroup:
   if (message.member.roles.has(adminRole.id).catch(console.warn)) userrole = "adminrole";
-  continue noadmingroup;
+  continue noadmingroup:
 
   // get id of mod role (if set up), if not disable mod group and skip to end of perm codeblock.
   const modRole = message.guild.roles.find("name", permsjson.modgroup);
-  if (modRole.id == "null") console.error("mod group not found, mod commands disabled") break nomodgroup;
+  if (modRole.id == "null") console.error("mod group not found, mod commands disabled")
+  if (modRole.id == "null") break nomodgroup;
   if (message.member.roles.has(modRole.id).catch(console.warn)) userrole = "modrole";
-  continue nomodgroup;
-  continue nopermgroups;
+  continue nomodgroup:
+  continue nopermgroups:
 
   const args = message.content.split(" ");
   const command = args.shift().slice(configjson.prefix.length);
