db0177fb54b2ca1bac0f29cb597c6e6192187877
hackerjef <auguststenz@aol.com>
2017-11-15 01:47:27 -0500

    e

--- src/Functions/update.js
+++ src/Functions/update.js
@@ -1,15 +1,12 @@
 exports.check = (path) => {
-  require('simple-git')(path).fetch("remote").exec(console.log("checking for updates!"));
+  require("simple-git")(path).fetch("remote").exec(console.log("checking for updates!"));
   var git = require("git-utils");
   //get ahead or behild count
-  let repo = git.open(path)
+  let repo = git.open(path);
   let gitdata = repo.getAheadBehindCount("master");
-  console.log(gitdata)
   if (gitdata.behind >=1) {
-    console.log("1")
     return true;
   } else {
-    console.log("9")
     return false;
   }
 };
